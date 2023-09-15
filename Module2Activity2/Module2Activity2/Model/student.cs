de2f0cbf3426e0bfbe4605e2786f98dbb5628a99
User <User@B29>
2023-09-15 11:18:51 +0800

    Add project files.

---
+++ Module2Activity2/Module2Activity2/Model/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module2Activity2.Model
+{
+    class student
+    {
+
+        public string Subjectcode { get; set; }
+        public string SubjectTitle { get; set; }
+        public int Unit { get; set; }
+
+    }
+}
---
+++ Module2Activity2/Module2Activity2/View/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module2Activity2.ViewModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module2Activity2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new StudentViewModel();
+        }
+    }
+}
