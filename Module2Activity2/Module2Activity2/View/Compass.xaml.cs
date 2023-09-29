278b21d837e9fad8864143efc876261186b40219
ten <101978567+strcurseten@users.noreply.github.com>
2023-09-29 18:18:46 +0800

    compass and gesture

---
+++ Module2Activity2/Module2Activity2/View/Compass.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Module02_Activity02.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module02_Activity02.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Compass : ContentPage
+    {
+        public Compass()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (!DesignMode.IsDesignModeEnabled)
+                ((MyCompassViewModel)BindingContext).StartCommand.Execute(null);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (!DesignMode.IsDesignModeEnabled)
+                ((MyCompassViewModel)BindingContext).StopCommand.Execute(null);
+        }
+    }
+}
---
+++ Module2Activity2/Module2Activity2/ViewModel/MyCompassViewModel.cs
@@ -0,0 +1,60 @@
+﻿using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Module02_Activity02.ViewModel
+{
+    public class MyCompassViewModel : MvvmHelpers.BaseViewModel
+    {
+        public MyCompassViewModel()
+        {
+            StopCommand = new Command(Stop);
+            StartCommand = new Command(Start);
+        }
+
+        string headingDisplay;
+
+        public string HeadingDisplay
+        {
+            get => headingDisplay;
+            set => SetProperty(ref headingDisplay, value);
+        }
+
+        double heading = 0;
+
+        public double Heading
+        {
+            get => heading;
+            set => SetProperty(ref heading, value);
+        }
+
+        public Command StopCommand { get; }
+
+        void Stop()
+        {
+            if (!Compass.IsMonitoring)
+                return;
+            Compass.ReadingChanged -= Compass_ReadingChanged;
+            Compass.Stop();
+        }
+
+        public Command StartCommand { get; }
+
+        void Start()
+        {
+            if (Compass.IsMonitoring)
+                return;
+            Compass.ReadingChanged += Compass_ReadingChanged;
+            Compass.Start(SensorSpeed.UI, true);
+        }
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            Heading = e.Reading.HeadingMagneticNorth;
+            HeadingDisplay = $"Heading: {Heading.ToString()}";
+        }
+    }
+}
